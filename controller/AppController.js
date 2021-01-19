9a15619657f82a2cbed6a7bf23836a30b8cc69c3
khoaiz Ahmmed Tusher <60465549+khoaizahmmed@users.noreply.github.com>
2021-01-19 12:32:59 +0600

    Update AppController.js
    comment database

--- controller/AppController.js
+++ controller/AppController.js
@@ -1,9 +1,9 @@
-const Table = require('../model/Table')
+// const Table = require('../model/Table')
 
 const AppController ={
     index : async (req,res)=>{
-        const rows = await Table.data()
-        console.log(rows)
+//         const rows = await Table.data()
+//         console.log(rows)
         res.render('')
     }
 }
